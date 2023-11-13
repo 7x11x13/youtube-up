1e9377d38caa2b2a81dc22a11a75906678d0a7f4
7x11x13 <x7x11x13@gmail.com>
2023-11-12 19:14:17 -0500

    Initial commit

---
+++ schema.py
@@ -0,0 +1,481 @@
+import datetime
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Optional
+
+from dataclasses_json import config, dataclass_json
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIClient:
+    clientName: int = 62
+    clientVersion: str = "1.20231103.02.01"
+    experimentsToken: str = ""
+    gl: str = "US"
+    hl: str = "en"
+    utcOffsetMinutes: int = -300
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APISessionInfo:
+    token: str = ""
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIRequest:
+    internalExperimentFlags: list = ()
+    returnLogEntry: bool = True
+    sessionInfo: APISessionInfo = APISessionInfo()
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIRoleType:
+    channelRoleType: str = "CREATOR_CHANNEL_ROLE_TYPE_OWNER"
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIDelegationContext:
+    externalChannelId: str
+    roleType: APIRoleType = APIRoleType()
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUser:
+    delegationContext: APIDelegationContext
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIContext:
+    client: APIClient
+    request: APIRequest
+    user: APIUser
+
+    @classmethod
+    def from_session_data(cls, channel_id: str, session_token: str):
+        return cls(
+            APIClient(),
+            APIRequest(sessionInfo=APISessionInfo(session_token)),
+            APIUser(APIDelegationContext(channel_id)),
+        )
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIID:
+    id: str
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIScottyResourceID:
+    scottyResourceId: APIID
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIMetadataTitle:
+    newTitle: str
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIMetadataDescription:
+    newDescription: str
+    shouldSegment: bool = True
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIMetadataPrivacy:
+    newPrivacy: str
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIMetadataDraftState:
+    isDraft: bool
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIMetadataTags:
+    newTags: list[str]
+
+
+class UpdateMetadataBase:
+    @classmethod
+    def from_metadata_args(cls, *args):
+        if all(a is None for a in args):
+            return None
+        else:
+            return cls(*args)
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataMadeForKids(UpdateMetadataBase):
+    newMfk: str
+    operation: str = "MDE_MADE_FOR_KIDS_UPDATE_OPERATION_SET"
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataPrivacy(UpdateMetadataBase):
+    newPrivacy: str
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataCategory(UpdateMetadataBase):
+    newCategoryId: int
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataCommentOptions(UpdateMetadataBase):
+    newAllowComments: Optional[bool] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    newAllowCommentsMode: Optional[str] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    newCanViewRatings: Optional[bool] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    newDefaultSortOrder: Optional[str] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataDistributionOptions(UpdateMetadataBase):
+    newAllowEmbedding: bool
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataLicense(UpdateMetadataBase):
+    newLicenseId: str
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataPublishingOptions(UpdateMetadataBase):
+    newPostToFeed: bool
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIDate:
+    day: int
+    month: int
+    year: int
+
+    @classmethod
+    def from_date(cls, date: datetime.datetime):
+        if date is None:
+            return None
+        else:
+            return cls(date.day, date.month, date.year)
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataRecordedDate(UpdateMetadataBase):
+    newRecordedDate: APIDate
+    operation: str = "MDE_RECORDED_DATE_UPDATE_OPERATION_SET"
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataAudioLanguage(UpdateMetadataBase):
+    newAudioLanguage: str
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataAutoChapter(UpdateMetadataBase):
+    creatorOptOut: bool
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataAutoPlaces(UpdateMetadataBase):
+    creatorOptOut: bool
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataAutoLearningConcepts(UpdateMetadataBase):
+    autoConceptsCreatorOptOut: bool
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataProductPlacement(UpdateMetadataBase):
+    newHasPaidProductPlacement: Optional[bool] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    newShowPaidProductPlacementOverlay: Optional[bool] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataRacy(UpdateMetadataBase):
+    newRacy: str
+    operation: str = "MDE_RACY_UPDATE_OPERATION_SET"
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIUpdateMetadataRemoveDraftState(UpdateMetadataBase):
+    operation: str = "MDE_DRAFT_STATE_UPDATE_OPERATION_REMOVE_DRAFT_STATE"
+
+
+class PrivacyEnum(str, Enum):
+    PRIVATE = "PRIVATE"
+    UNLISTED = "UNLISTED"
+    PUBLIC = "PUBLIC"
+
+
+class CategoryEnum(int, Enum):
+    FILM_ANIMATION = 1
+    AUTOS_VEHICLES = 2
+    MUSIC = 10
+    PETS_ANIMALS = 15
+    SPORTS = 17
+    TRAVEL_EVENTS = 19
+    GAMING = 20
+    PEOPLE_BLOGS = 22
+    COMEDY = 23
+    ENTERTAINMENT = 24
+    NEWS_POLITICS = 25
+    HOWTO_STYLE = 26
+    EDUCATION = 27
+    SCIENCE_TECH = 28
+    NONPROFITS_ACTIVISM = 29
+
+
+class LanguageEnum(str, Enum):
+    ENGLISH = "en"
+
+
+class LicenseEnum(str, Enum):
+    STANDARD = "standard"
+    CREATIVE_COMMONS = "creative_commons"
+
+
+class AllowCommentsEnum(str, Enum):
+    ALL_COMMENTS = "ALL_COMMENTS"
+    HOLD_INAPPROPRIATE = "AUTOMATED_COMMENTS"
+    HOLD_INAPPROPRIATE_STRICT = "AUTO_MODERATED_COMMENTS_HOLD_MORE"
+    HOLD_ALL = "APPROVED_COMMENTS"
+
+
+class CommentsSortOrderEnum(str, Enum):
+    LATEST = "MDE_COMMENT_SORT_ORDER_LATEST"
+    TOP = "MDE_COMMENT_SORT_ORDER_TOP"
+
+
+RacyDict = {
+    True: "MDE_RACY_TYPE_RESTRICTED",
+    False: "MDE_RACY_TYPE_NOT_RESTRICTED",
+    None: None,
+}
+MFKDict = {True: "MDE_MADE_FOR_KIDS_TYPE_MFK", False: "MDE_MADE_FOR_KIDS_TYPE_NOT_MFK"}
+
+
+@dataclass(frozen=True)
+class Metadata:
+    title: str
+    description: str
+    privacy: PrivacyEnum
+    made_for_kids: bool = False
+    tags: list[str] = ()
+    # optional metadata for update_metadata
+    publish_to_feed: Optional[bool] = None
+    category: Optional[CategoryEnum] = None
+    auto_chapter: Optional[bool] = None
+    auto_places: Optional[bool] = None
+    auto_concepts: Optional[bool] = None
+    has_product_placement: Optional[bool] = None
+    show_product_placement_overlay: Optional[bool] = None
+    recorded_date: Optional[datetime.date] = None
+    restriced_to_over_18: Optional[bool] = None
+    audio_language: Optional[LanguageEnum] = None
+    license: Optional[LicenseEnum] = None
+    allow_comments: Optional[bool] = None
+    allow_comments_mode: Optional[AllowCommentsEnum] = None
+    can_view_ratings: Optional[bool] = None
+    comments_sort_order: Optional[CommentsSortOrderEnum] = None
+    allow_embedding: Optional[bool] = None
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIInitialMetadata:
+    title: APIMetadataTitle
+    description: APIMetadataDescription
+    privacy: APIMetadataPrivacy
+    draftState: APIMetadataDraftState
+    tags: APIMetadataTags
+
+    @classmethod
+    def from_metadata(cls, metadata: Metadata):
+        return cls(
+            APIMetadataTitle(metadata.title),
+            APIMetadataDescription(metadata.description),
+            APIMetadataPrivacy(PrivacyEnum.PRIVATE),
+            APIMetadataDraftState(True),
+            APIMetadataTags(metadata.tags),
+        )
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIRequestCreateVideo:
+    channelId: str
+    context: APIContext
+    delegationContext: APIDelegationContext
+    frontendUploadId: str
+    initialMetadata: APIInitialMetadata
+    presumedShort: bool
+    resourceId: APIScottyResourceID
+
+    @classmethod
+    def from_session_data(
+        cls,
+        channel_id: str,
+        session_token: str,
+        front_end_upload_id: str,
+        metadata: Metadata,
+        presumed_short: bool,
+        scotty_resource_id: str,
+    ):
+        return cls(
+            channel_id,
+            APIContext.from_session_data(channel_id, session_token),
+            APIDelegationContext(channel_id),
+            front_end_upload_id,
+            APIInitialMetadata.from_metadata(metadata),
+            presumed_short,
+            APIScottyResourceID(APIID(scotty_resource_id)),
+        )
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class APIRequestUpdateMetadata:
+    context: APIContext
+    delegationContext: APIDelegationContext
+    encryptedVideoId: str
+    madeForKids: APIUpdateMetadataMadeForKids
+    draftState: APIUpdateMetadataRemoveDraftState
+    privacyState: APIUpdateMetadataPrivacy
+    # optional updates
+    autoChapter: Optional[APIUpdateMetadataAutoChapter] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    autoPlaces: Optional[APIUpdateMetadataAutoPlaces] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    learningConcepts: Optional[APIUpdateMetadataAutoLearningConcepts] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    productPlacement: Optional[APIUpdateMetadataProductPlacement] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    racy: Optional[APIUpdateMetadataRacy] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    audioLanguage: Optional[APIUpdateMetadataAudioLanguage] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    recordedDate: Optional[APIUpdateMetadataRecordedDate] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    category: Optional[APIUpdateMetadataCategory] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    commentOptions: Optional[APIUpdateMetadataCommentOptions] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    distributionOptions: Optional[APIUpdateMetadataDistributionOptions] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    license: Optional[APIUpdateMetadataLicense] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+    publishingOptions: Optional[APIUpdateMetadataPublishingOptions] = field(
+        default=None, metadata=config(exclude=lambda x: x is None)
+    )
+
+    @classmethod
+    def from_session_data(
+        cls,
+        channel_id: str,
+        session_token: str,
+        encrypted_video_id: str,
+        metadata: Metadata,
+    ):
+        return cls(
+            APIContext.from_session_data(channel_id, session_token),
+            APIDelegationContext(channel_id),
+            encrypted_video_id,
+            APIUpdateMetadataMadeForKids(MFKDict[metadata.made_for_kids]),
+            APIUpdateMetadataRemoveDraftState(),
+            APIUpdateMetadataPrivacy(metadata.privacy),
+            APIUpdateMetadataAutoChapter.from_metadata_args(metadata.auto_chapter),
+            APIUpdateMetadataAutoPlaces.from_metadata_args(metadata.auto_places),
+            APIUpdateMetadataAutoLearningConcepts.from_metadata_args(
+                metadata.auto_concepts
+            ),
+            APIUpdateMetadataProductPlacement.from_metadata_args(
+                metadata.has_product_placement, metadata.show_product_placement_overlay
+            ),
+            APIUpdateMetadataRacy.from_metadata_args(
+                RacyDict[metadata.restriced_to_over_18]
+            ),
+            APIUpdateMetadataAudioLanguage.from_metadata_args(metadata.audio_language),
+            APIUpdateMetadataRecordedDate.from_metadata_args(
+                APIDate.from_date(metadata.recorded_date)
+            ),
+            APIUpdateMetadataCategory.from_metadata_args(metadata.category),
+            APIUpdateMetadataCommentOptions.from_metadata_args(
+                metadata.allow_comments,
+                metadata.allow_comments_mode,
+                metadata.can_view_ratings,
+                metadata.comments_sort_order,
+            ),
+            APIUpdateMetadataDistributionOptions.from_metadata_args(
+                metadata.allow_embedding
+            ),
+            APIUpdateMetadataLicense.from_metadata_args(metadata.license),
+            APIUpdateMetadataPublishingOptions.from_metadata_args(
+                metadata.publish_to_feed
+            ),
+        )
+
+
+if __name__ == "__main__":
+    x = Metadata(
+        "test title",
+        "test description",
+        PrivacyEnum.UNLISTED,
+        publish_to_feed=False,
+        allow_comments=False,
+    )
+    y = APIRequestUpdateMetadata.from_session_data(
+        "channel_id", "sessiontoken", "videoid", x
+    )
+    print(y.to_dict())
---
+++ uploader.py
@@ -0,0 +1,236 @@
+import copy
+import json
+import math
+import re
+import time
+import uuid
+from hashlib import sha1
+from http.cookiejar import Cookie, MozillaCookieJar
+
+import requests
+from seleniumwire import webdriver
+from seleniumwire.utils import decode
+
+from schema import *
+
+
+class YTUploaderSession:
+    innertube_api_key_regex = re.compile(r'"INNERTUBE_API_KEY":"([^"]*)"')
+    channel_id_regex = re.compile(r"https://studio.youtube.com/channel/([^/]*)/*")
+
+    def __init__(self, cookies_txt_file: str):
+        self.channel_id: str = ""
+        self.innertube_api_key: str = ""
+        self.session_token: str = ""
+        self.front_end_upload_id: str = ""
+
+        # load cookies and init session
+        self.cookies = MozillaCookieJar(cookies_txt_file)
+        self.cookies.load(ignore_discard=True)
+        self.session = requests.Session()
+        for cookie in self.cookies:
+            if cookie.name == "YT_UPLOADER_SESSION_ID":
+                self.session_token = cookie.value
+            else:
+                self.session.cookies.set_cookie(copy.copy(cookie))
+        self.session.headers = {
+            "Authorization": f"SAPISIDHASH {self._generateSAPISIDHASH(self.session.cookies['SAPISID'])}",
+            "x-origin": "https://studio.youtube.com",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0",
+        }
+
+    def upload(self, file_path: str, metadata: Metadata):
+        self._get_session_data()
+        url = self._get_upload_url()
+        scotty_resource_id = self._upload_file(url, file_path)
+        try:
+            encrypted_video_id = self._create_video(scotty_resource_id, metadata)
+        except requests.HTTPError as ex:
+            if ex.response.status_code == 400:
+                # could be bad session token, try to get new one
+                self._get_session_token()
+                encrypted_video_id = self._create_video(scotty_resource_id, metadata)
+        self._update_metadata(encrypted_video_id, metadata)
+
+    def _get_session_token(self):
+        try:
+            # try firefox
+            options = webdriver.FirefoxOptions()
+            options.add_argument("--headless")
+            driver = webdriver.Firefox(options=options)
+        except Exception:
+            try:
+                # try chrome
+                options = webdriver.ChromeOptions()
+                options.add_argument("--headless=new")
+                driver = webdriver.Chrome(options=options)
+            except Exception:
+                raise Exception(
+                    "Could not launch Firefox or Chrome. Make sure geckodriver or chromedriver is installed"
+                )
+
+        driver.get("https://youtube.com")
+
+        for cookie in self.cookies:
+            driver.add_cookie(cookie.__dict__)
+
+        driver.get("https://youtube.com/upload")
+
+        if "studio.youtube.com/channel" not in driver.current_url:
+            driver.quit()
+            raise Exception(
+                "Could not log in to YouTube account. Try getting new cookies"
+            )
+
+        r = driver.wait_for_request("studio.youtube.com/youtubei/v1/ars/grst")
+        response = r.response
+        r_json = json.loads(
+            decode(response.body, response.headers.get("Content-Encoding"))
+        )
+        self.session_token = r_json["sessionToken"]
+        self.cookies.set_cookie(
+            Cookie(
+                None,
+                "YT_UPLOADER_SESSION_ID",
+                self.session_token,
+                None,
+                False,
+                "",
+                False,
+                False,
+                "",
+                False,
+                False,
+                None,
+                False,
+                None,
+                None,
+                {},
+            )
+        )
+        self.cookies.save()
+        driver.quit()
+
+    @staticmethod
+    def _generateUUID() -> str:
+        return str(uuid.uuid4()).upper()
+
+    @staticmethod
+    def _generateSAPISIDHASH(SAPISID) -> str:
+        timestamp = math.floor(time.time())
+        msg = f"{timestamp} {SAPISID} {'https://studio.youtube.com'}"
+        hash = sha1(msg.encode("utf-8")).hexdigest()
+        return f"{timestamp}_{hash}"
+
+    def _get_session_data(self):
+        r = self.session.get("https://youtube.com/upload")
+
+        if "studio.youtube.com/channel" not in r.url:
+            raise Exception(
+                "Could not log in to YouTube account. Try getting new cookies"
+            )
+
+        self.channel_id = self.channel_id_regex.match(r.url).group(1)
+        self.innertube_api_key = self.innertube_api_key_regex.search(r.text).group(1)
+
+    def _get_creator_channels(self) -> list[str]:
+        params = {"key": self.innertube_api_key, "alt": "json"}
+        data = {
+            "channelIds": [self.channel_id],
+            "context": APIContext.from_session_data(
+                self.channel_id, self.session_token
+            ).to_dict(),
+        }
+        r = self.session.post(
+            "https://studio.youtube.com/youtubei/v1/creator/get_creator_channels",
+            params=params,
+            json=data,
+        )
+        r.raise_for_status()
+        return r.json()
+
+    def _get_upload_url(self) -> str:
+        params = {"authuser": 0}
+        headers = {
+            "x-goog-upload-command": "start",
+            "x-goog-upload-protocol": "resumable",
+        }
+        self.front_end_upload_id = f"innertube_studio:{self._generateUUID()}:0"
+        r = self.session.post(
+            "https://upload.youtube.com/upload/studio",
+            headers=headers,
+            params=params,
+            data=f'{{"frontendUploadId":"{self.front_end_upload_id}"}}',
+        )
+        r.raise_for_status()
+        upload_url = r.headers["x-goog-upload-url"]
+        return upload_url
+
+    def _upload_file(self, upload_url: str, file_path: str):
+        params = {"authuser": 0}
+        headers = {
+            "x-goog-upload-command": "upload, finalize",
+            "x-goog-upload-offset": "0",
+        }
+        with open(file_path, "rb") as f:
+            r = self.session.post(upload_url, headers=headers, params=params, data=f)
+
+        r.raise_for_status()
+        return r.json()["scottyResourceId"]
+
+    def _create_video(self, scotty_resource_id: str, metadata: Metadata) -> str:
+        params = {"key": self.innertube_api_key, "alt": "json"}
+        """
+        data = {
+            "channelId": self.channel_id,
+            "context": APIContext.from_session_data(
+                self.channel_id, self.session_token
+            ).to_dict(),
+            "delegationContext": APIDelegationContext(self.channel_id).to_dict(),
+            "frontendUploadId": self.front_end_upload_id,
+            "initialMetadata": APIInitialMetadata.from_metadata(metadata).to_dict(),
+            "presumedShort": False,
+            "resourceId": {"scottyResourceId": {"id": scotty_resource_id}},
+        }
+        """
+        data = APIRequestCreateVideo.from_session_data(
+            self.channel_id,
+            self.session_token,
+            self.front_end_upload_id,
+            metadata,
+            False,
+            scotty_resource_id,
+        ).to_dict()
+        print(data)
+        r = self.session.post(
+            "https://studio.youtube.com/youtubei/v1/upload/createvideo",
+            params=params,
+            json=data,
+        )
+        r.raise_for_status()
+        return r.json()["videoId"]
+
+    def _update_metadata(self, encrypted_video_id: str, metadata: Metadata):
+        params = {"key": self.innertube_api_key, "alt": "json"}
+        data = APIRequestUpdateMetadata.from_session_data(
+            self.channel_id, self.session_token, encrypted_video_id, metadata
+        ).to_dict()
+        print(data)
+        r = self.session.post(
+            "https://studio.youtube.com/youtubei/v1/video_manager/metadata_update",
+            params=params,
+            json=data,
+        )
+        r.raise_for_status()
+
+
+if __name__ == "__main__":
+    uploader = YTUploaderSession("cookies.txt")
+    file_path = "test.mp4"
+    metadata = Metadata(
+        "test title",
+        "test description",
+        PrivacyEnum.UNLISTED,
+        tags=["Music", "test tag lol"],
+    )
+    uploader.upload(file_path, metadata)
